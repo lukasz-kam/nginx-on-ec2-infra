31e6f312e3b1c0f820c24e730231d1c5df64be52
lukasz-kam <lukasz.kaminski02@gmail.com>
2025-07-18 18:18:20 +0200

    feat: Create IGW and a route table, attach route table to public subnet

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
- tags = {
-    Name = var.vpc_name
+  tags = {
+    Name      = var.vpc_name
     ManagedBy = "Terraform"
   }
 }
@@ -20,4 +20,38 @@ resource "aws_subnet" "public" {
   cidr_block        = "10.0.1.0/24"
   availability_zone = var.subnet_az
 
+  tags = {
+    Name      = "${var.vpc_name}_PublicSubnet"
+    ManagedBy = "Terraform"
+  }
+
+}
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name      = "${var.vpc_name}_IGW"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name      = "${var.vpc_name}_PublicRT"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_route" "public_internet_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_route_table.id
 }
