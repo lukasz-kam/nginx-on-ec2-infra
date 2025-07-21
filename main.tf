fc1097e6b657839e2951b0f7d5455d7f930416a4
lukasz-kam <lukasz.kaminski02@gmail.com>
2025-07-21 16:00:15 +0200

    feat: Change configuration to download image from S3 by EC2 instance instead of accessing it by public url

--- main.tf
+++ main.tf
@@ -27,48 +27,29 @@ module "my_s3_bucket" {
 module "my_ec2_instance" {
   source = "git@git.epam.com:lukasz_kaminski1/terraform-modules.git//modules/ec2?ref=main"
 
-  instance_name      = var.instance_name
-  ami_id             = data.aws_ami.amazon.id
-  instance_type      = var.instance_type
-  subnet_id          = aws_subnet.public.id
-  assign_public_ip   = true
-  security_group_ids = [aws_security_group.nginx_sg.id]
-  # iam_instance_profile = aws_iam_instance_profile.ec2_s3_profile.name
+  instance_name        = var.instance_name
+  ami_id               = data.aws_ami.amazon.id
+  instance_type        = var.instance_type
+  subnet_id            = aws_subnet.public.id
+  assign_public_ip     = true
+  security_group_ids   = [aws_security_group.nginx_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_s3_profile.name
 
   user_data = templatefile("${path.module}/scripts/user_data_script.sh", {
     S3_BUCKET_NAME = module.my_s3_bucket.bucket_name
-    S3_IMAGE_URL   = local.image_public_url
+    S3_IMAGE_KEY   = local.object_key
   })
 }
 
 locals {
   source_file_path = "./images/logo.png"
   object_key       = "logo.png"
-  image_public_url = "https://${module.my_s3_bucket.bucket_name}.s3.${var.aws_region}.amazonaws.com/public/${local.object_key}"
 }
-
 
 resource "aws_s3_object" "file_upload" {
   bucket = module.my_s3_bucket.bucket_name
   key    = "public/${local.object_key}"
   source = local.source_file_path
-}
-
-resource "aws_s3_bucket_policy" "allow_public_read" {
-  depends_on = [module.my_s3_bucket]
-  bucket     = module.my_s3_bucket.bucket_name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${module.my_s3_bucket.s3_bucket_arn}/public/*"
-      },
-    ],
-  })
 }
 
 resource "aws_security_group" "nginx_sg" {
@@ -148,62 +129,62 @@ resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-# resource "aws_iam_policy" "s3_read_policy" {
-#   name        = "s3-read-only-policy"
-#   description = "Allows EC2 instance to read objects from a specific S3 bucket"
+resource "aws_iam_policy" "s3_read_policy" {
+  name        = "s3-read-only-policy"
+  description = "Allows EC2 instance to read objects from a specific S3 bucket"
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "s3:GetObject",
-#           "s3:ListBucket"
-#         ]
-#         Resource = [
-#           module.my_s3_bucket.s3_bucket_arn,
-#           "${module.my_s3_bucket.s3_bucket_arn}/*"
-#         ]
-#       },
-#     ]
-#   })
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          module.my_s3_bucket.s3_bucket_arn,
+          "${module.my_s3_bucket.s3_bucket_arn}/*"
+        ]
+      },
+    ]
+  })
 
-#   tags = {
-#     ManagedBy = "Terraform"
-#   }
-# }
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
 
-# resource "aws_iam_role" "ec2_s3_role" {
-#   name = "ec2-s3-read-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "ec2-s3-read-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
-#   tags = {
-#     ManagedBy = "Terraform"
-#   }
-# }
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
 
-# resource "aws_iam_role_policy_attachment" "s3_read_attachment" {
-#   role       = aws_iam_role.ec2_s3_role.name
-#   policy_arn = aws_iam_policy.s3_read_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "s3_read_attachment" {
+  role       = aws_iam_role.ec2_s3_role.name
+  policy_arn = aws_iam_policy.s3_read_policy.arn
+}
 
-# resource "aws_iam_instance_profile" "ec2_s3_profile" {
-#   name = "ec2-s3-read-profile"
-#   role = aws_iam_role.ec2_s3_role.name
+resource "aws_iam_instance_profile" "ec2_s3_profile" {
+  name = "ec2-s3-read-profile"
+  role = aws_iam_role.ec2_s3_role.name
 
-#   tags = {
-#     ManagedBy = "Terraform"
-#   }
-# }
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
--- scripts/user_data_script.sh
+++ scripts/user_data_script.sh
@@ -2,8 +2,9 @@
 
 sudo yum update -y
 sudo yum install nginx -y
+aws s3 cp "s3://${S3_BUCKET_NAME}/public/${S3_IMAGE_KEY}" "/usr/share/nginx/html/${S3_IMAGE_KEY}"
 
-sudo bash -c "cat << EOF_HTML > /usr/share/nginx/html/index.html
+sudo bash -c "cat << 'EOF_HTML' > /usr/share/nginx/html/index.html
   <!DOCTYPE html>
   <html>
   <head>
@@ -15,8 +16,8 @@ sudo bash -c "cat << EOF_HTML > /usr/share/nginx/html/index.html
   </head>
   <body>
       <h1>Nginx on EC2</h1>
-      <p>Image from S3 bucket:</p>
-      <img src="${S3_IMAGE_URL}" >
+      <p>Image downloaded from S3:</p>
+      <img src=${S3_IMAGE_KEY} >
   </body>
   </html>
 EOF_HTML"
