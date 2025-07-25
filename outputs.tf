2b93f0916b3d6a6828b2458aaab5efdf7c57f2b9
lukasz-kam <lukasz.kaminski02@gmail.com>
2025-07-25 11:06:22 +0200

    feat: Create route53 hosted zone and A record for the nginx server

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance (if assigned)."
   value       = module.my_ec2_instance.instance_public_ip
 }
+
+output "fqdn_www" {
+  description = "The fully qualified domain name for the nginx server."
+  value       = aws_route53_record.www.name
+}
---
+++ route53.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+  ttl     = "60"
+  records = [module.my_ec2_instance.instance_public_ip]
+}
