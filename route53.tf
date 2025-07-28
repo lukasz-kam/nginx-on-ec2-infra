a0f6cdc9aa4526db8de0f5ca59d1e7718cd567cb
lukasz-kam <lukasz.kaminski02@gmail.com>
2025-07-28 14:35:23 +0200

    feat: Change the nginx index page to show instance private IP with the use of meta-data

--- route53.tf
+++ route53.tf
@@ -9,7 +9,7 @@ resource "aws_route53_zone" "main" {
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.${var.domain_name}"
-  type    = "A"
-  ttl     = "60"
+  type    = var.www_record_type
+  ttl     = var.www_record_ttl
   records = [module.my_ec2_instance.instance_public_ip]
 }
